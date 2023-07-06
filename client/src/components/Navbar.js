460540944948175388439ab37058ae95ffb73a51
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-05 22:50:28 -0500

    added the Logout functionality to the Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,31 @@
 import { Container } from '@mui/material';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { user, setUser } = props;
+
     const linkStyle = {
         color: "#000",
         fontSize: "1.5rem",
         fontWeight: "bold",
         margin: "0 0.5rem",
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        setUser(null);
+        axios
+            .get('http://localhost:8000/api/users/logout', { withCredentials: true })
+            .then((res) => {
+                document.location.reload();
+                console.log(res);
+            }
+            )
+            .catch((err) => {
+                console.log(err);
+            }
+            );
     }
 
     return (
@@ -35,9 +54,11 @@ const Navbar = () => {
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/register" style={linkStyle}>
-                            Sign up
-                        </Link>
+                        {user ?
+                            <Link className="nav-link" onClick={logout} style={linkStyle}>
+                                Logout
+                            </Link> : ""
+                        }
                     </li>
                 </ul>
             </Container>
