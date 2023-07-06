6f4677ad87c9d5fd38d1730962d85c47732a337e
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-05 21:42:46 -0500

    Details page design and functionality

--- client/src/views/Details.js
+++ client/src/views/Details.js
@@ -1,8 +1,25 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import styled from 'styled-components';
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
+const StyledButton = styled.button`
+margin:0 auto;
+margin-top:40px;
+margin-bottom:15px;
+padding: 7px;
+border: 1px solid rgba(0, 0, 0, 0);
+border-radius: 5px;
+background: #fff;
+width: 330px;
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+
+&:hover {
+    background-color: #56d0f0;
+    border: 1px solid #aaf;
+}
+`;
 const Details = () => {
     const [post, setPost] = useState({})
     let { id } = useParams()
@@ -20,13 +37,12 @@ const Details = () => {
         <div className='container mt-5'>
             <div className="row">
                 <div className='col-5'>
-                    <h1>{post.title}</h1>
-                    <img src={post.Url} alt={post.title} className='w-100' />
-                    <Link to={`/edit/${post._id}`} className="btn btn-primary mt-3">Edit</Link>
+                    <img src={post.Url} alt={post.title} className='w-100 h-100vh' />
                 </div>
                 <div className='col-7'>
-                    <h3>Details</h3>
+                    <h1>{post.title}</h1>
                     <p>{post.description}</p>
+                    <StyledButton><Link to={`/edit/${post._id}`}>Edit</Link></StyledButton>
                 </div>
             </div>
         </div>
