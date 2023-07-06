d7192e22cacb1118884b9383eab25a087b2660df
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-06 02:17:17 -0500

    fixed path bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
-import { useEffect, useState } from 'react';
-import Schedule from './views/Schedule';
+import { useEffect, useRef, useState } from 'react';
 import Login from './components/Login';
 import Details from './views/Details';
 import Update from './views/Update';
@@ -9,6 +8,7 @@ import Main from './views/Main';
 import Add from './views/Add';
 import axios from 'axios';
 import './App.css';
+import Calender from './views/Calender';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -25,13 +25,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/schedule" element={<Calender />} />
+        <Route path="/" element={<Main user={user} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/add" element={<Add user={user} />} />
         <Route path="/edit/:id" element={<Update user={user} />} />
         <Route path="/detail/:id" element={<Details />} />
-        <Route path="/schedule" element={<Schedule />} />
+
       </Routes>
     </div>
   );
