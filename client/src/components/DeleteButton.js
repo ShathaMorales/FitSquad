2d1c2c6e1a9dd60d16a6f9cb413e8ce850893fca
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-05 19:51:06 -0500

    "Delete Button" Functionality

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import axios from 'axios'
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+margin:0 auto;
+margin-top:20px;
+// margin-bottom:15px;
+padding: 7px;
+border: 1px solid rgba(0, 0, 0, 0);
+border-radius: 5px;
+background: #fff;
+width: 330px;
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+
+&:hover {
+    background-color: #84214d;
+    border: 1px solid #aaf;
+}
+`;
 
 const DeleteButton = (props) => {
     const { id, successDelete } = props;
 
     const deletePost = e => {
+        e.preventDefault();
         console.log(id)
         axios.delete('http://localhost:8000/api/posts/' + id)
             .then(res => {
@@ -14,9 +33,9 @@ const DeleteButton = (props) => {
     }
 
     return (
-        <button className='btn btn-danger' onClick={deletePost}>
+        <StyledButton onClick={deletePost}>
             Delete
-        </button>
+        </StyledButton>
     )
 }
 
