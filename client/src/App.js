08bd2b8745468e91c621776c61c92ea5f64e235b
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-05 19:51:59 -0500

    "Form" update

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,31 @@ import Main from './views/Main';
 import Update from './views/Update';
 import Details from './views/Details';
 import Add from './views/Add';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/users/loggedin", { withCredentials: true })
+      .then(res => {
+        console.log(res.data.user)
+        console.log(res.data.user._id)
+        setUser(res.data.user)
+      })
+      .catch(err => console.log(err))
+
+  }, [])
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/edit/:id" element={<Update />} />
+        <Route path="/add" element={<Add user={user} />} />
+        <Route path="/edit/:id" element={<Update user={user} />} />
         <Route path="/detail/:id" element={<Details />} />
       </Routes>
     </div>
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { useEffect } from 'react';
+import Select from 'react-select';
+import axios from 'axios';
+import DeleteButton from './DeleteButton';
+
 
 const FormContainer = styled.div`
-display: grid;
-grid-template-columns: 1fr minmax(200px, 400px) 1fr;
-grid-template-rows: 1fr minmax(auto, 1fr) 1fr;
-grid-gap: 10px;
-width: 100%;
-height: 100vh;
-background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
-background-size: 400% 400%;
-// animation: Gradient 15s ease infinite;
-box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
+
+
+// const FormContainer = styled.div`
+// display: grid;
+// grid-template-columns: 1fr minmax(200px, 400px) 1fr;
+// grid-template-rows: 1fr minmax(auto, 1fr) 1fr;
+// grid-gap: 10px;
+// width: 100%;
+// height: 100vh;
+// background-image:"https://ik.imagekit.io/shadid/young-beautiful-woman-bright-sportwear-isolated-gradient-pink-blue-background-neon-light.jpg?updatedAt=1688593548828"
+// // background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+// background-size: 400% 400%;
+// // animation: Gradient 15s ease infinite;
+// box-sizing: border-box;
+// `;
 
 const StyledForm = styled.form`
 grid-column: 2;
@@ -23,14 +40,16 @@ display: grid;
 grid-gap: 10px;
 margin: auto 0;
 padding: 20px;
+margin-right: 350px;
 background-color: rgba(255, 255, 255, 0.9);
 border-radius: 10px;
 box-shadow: 0 32px 64px rgba(0, 0, 0, 0.2);
 `;
 
 const StyledInput = styled.input`
+margin:0 auto;
 padding: 5px;
-margin: 10px;
+margin-bottom: 10px;
 border: 1px solid #ddd;
 border-radius: 5px;
 &:hover {
@@ -39,7 +58,10 @@ border-radius: 5px;
 `;
 
 const StyledButton = styled.button`
-padding: 10px;
+margin:0 auto;
+margin-top:40px;
+margin-bottom:15px;
+padding: 7px;
 border: 1px solid rgba(0, 0, 0, 0);
 border-radius: 5px;
 background: #fff;
@@ -47,16 +69,20 @@ width: 330px;
 box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
 
 &:hover {
-    background-color: #eef;
+    background-color: #56d0f0;
     border: 1px solid #aaf;
 }
 `;
 
 const Form = (props) => {
-
+  const { pic } = props;
+  const { user } = props;
   const [title, setTitle] = React.useState('');
   const [Url, setUrl] = React.useState('');
   const [description, setDescription] = React.useState('');
+  const [day, setDay] = React.useState('');
+  const { isUpdate } = props;
+  const { id } = useParams()
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -64,20 +90,25 @@ const Form = (props) => {
       title,
       Url,
       description,
-      user: '64a4c5a961b8bfa4c3e42fcd',
+      user: user._id,
+      day: day.valueو
+
     })
   }
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   useEffect(() => {
-    if (props.us) {
-      setTitle(props.us.title);
-      setUrl(props.us.Url);
-      setDescription(props.us.description);
+    if (props.post) {
+      setTitle(props.post.title);
+      setUrl(props.post.Url);
+      setDescription(props.post.description);
     }
-  }, [props.us]);
+  }, [props.post]);
+
+
 
   return (
-    <FormContainer >
+    <FormContainer style={{ backgroundImage: `url(${pic})` }}>
       <StyledForm onSubmit={onSubmitHandler} className="row">
         <div>
           <label htmlFor="title">Excersise Name</label>
@@ -90,7 +121,21 @@ const Form = (props) => {
             <label htmlFor="desc">Description</label>
             <StyledInput className="form-control" id="desc" rows="3" value={description} onChange={(e) => setDescription(e.target.value)}></StyledInput>
           </div>
+          <div>
+            <label htmlFor="desc">Day of the Week</label>
+            <Select
+              id="area"
+              value={day}
+              onChange={(selectedOption) => {
+                setDay(selectedOption);
+              }}
+              options={days.map((day) => {
+                return { value: day, label: day };
+              })}
+            />
+          </div>
         </div>
+        {isUpdate ? <DeleteButton id={id} /> : ""}
         <StyledButton type="submit" className="">{props.formAction}</StyledButton>
       </StyledForm>
       <p>{props.errors}</p>
