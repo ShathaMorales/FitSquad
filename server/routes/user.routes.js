cfb900425c15cbdf6b539fca2d5d6921d5fdf3bb
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-07-05 22:52:41 -0500

    fixed a problem with the logout route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,17 +1,17 @@
 const UserController = require('../controllers/user.controller');
 const PostController = require('../controllers/post.controller');
-const {authenticate } = require("../config/jwt.config");
+const { authenticate } = require("../config/jwt.config");
 
 module.exports = (app) => {
     app.post('/api/register', UserController.register);
     app.post('/api/login', UserController.login);
     app.get('/api/users', UserController.getAll);
-    app.get('/api/logout', UserController.logout);
-    app.get("/api/users/loggedin",authenticate, UserController.getLoggedInUser)
+    app.get('/api/users/logout', UserController.logout);
+    app.get("/api/users/loggedin", authenticate, UserController.getLoggedInUser)
     app.get('/api/user', UserController.findUser);
     app.post('/api/posts', PostController.createPost);
     app.get('/api/posts', PostController.getAllPosts);
     app.get('/api/posts/:id', PostController.getPost);
-    app.put('/api/posts/:id', PostController.updatePost);   
+    app.put('/api/posts/:id', PostController.updatePost);
     app.delete('/api/posts/:id', PostController.deletePost);
 }
